db58bdbc2099d20e277677d7f3b7c0ac648af33e
gab080305 <141954302+gab080305@users.noreply.github.com>
2025-10-17 23:56:53 +0000

    fix qualquer coisa

---
+++ build/CMakeFiles/generate_grpc_files.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_grpc_files"
+  "file_processor.grpc.pb.cc"
+  "file_processor.grpc.pb.h"
+  "file_processor.pb.cc"
+  "file_processor.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/generate_grpc_files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/servidor_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/servidor_cpp.dir/file_processor.grpc.pb.cc.o"
+  "CMakeFiles/servidor_cpp.dir/file_processor.grpc.pb.cc.o.d"
+  "CMakeFiles/servidor_cpp.dir/file_processor.pb.cc.o"
+  "CMakeFiles/servidor_cpp.dir/file_processor.pb.cc.o.d"
+  "CMakeFiles/servidor_cpp.dir/servidor.cpp.o"
+  "CMakeFiles/servidor_cpp.dir/servidor.cpp.o.d"
+  "file_processor.grpc.pb.cc"
+  "file_processor.grpc.pb.h"
+  "file_processor.pb.cc"
+  "file_processor.pb.h"
+  "servidor_cpp"
+  "servidor_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/servidor_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
